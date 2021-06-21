9e4b9a982f15a88469bfa031b9768eaccc5b0ef4
conall <conalldalydev@gmail.com>
2021-06-21 20:07:44 +0100

    separate function for parsing from files and sql directly

--- bigquery_validator/bigquery_validator.py
+++ bigquery_validator/bigquery_validator.py
@@ -11,13 +11,18 @@ warnings.filterwarnings("ignore", "Your application has authenticated using end 
 
 class BigQueryValidator:
 
-    def __init__(self):
+    def __init__(self,
+                 dry_run=True,
+                 use_query_cache=False):
         self.bq_client = bigquery.Client()
         self.params = self.load_params()
-        parser = argparse.ArgumentParser()
-        parser.add_argument('dir', nargs=1, default=None)
-        args = parser.parse_args()
-        print(args)
+        self.dry_run = dry_run
+        self.use_query_cache = use_query_cache
+
+        # Future work pass files to be parsed as arguments
+        # parser = argparse.ArgumentParser()
+        # parser.add_argument('dir', nargs=1, default=None)
+        # args = parser.parse_args()
 
     def load_params(self):
         from bigquery_validator.config import default_params
@@ -26,37 +31,50 @@ class BigQueryValidator:
         # TODO: future enhancement look for a global config file so that they don't need to be defined in each project
         logging.info(f'Looking for query_validator_config.py in {os.getcwd()}')
         if importlib.util.find_spec('query_validator_config') is not None:
-            from example.query_validator_config import params as extra_params
+            from query_validator_config import params as extra_params
             logging.info('Loading user defined params')
 
         params = {**default_params, **extra_params}
         return params
 
-    def validate_query(self, file_path):
+    def _render_templated_query(self, templated_query):
+        # Convert the Jinja templated SQL to a valid query
+        templated_query = templated_query.replace('params.', '')  # need this to get formatting correct
+        t = Template(templated_query)
+        return t.render(self.params)
+
+    def _dry_run_query(self, query):
+        # Construct a BigQuery client object.
+        job_config = bigquery.QueryJobConfig(dry_run=True, use_query_cache=False)
+
+        # Start the query, passing in the extra configuration.
+        # query_job =
+        query_job = self.bq_client.query(
+            query,
+            job_config=job_config,
+        )  # Make an API request.
+
+        # A dry run query completes immediately.
+        # todo: decide if we should print anything
+        logging.info("This query will process {} bytes.".format(query_job.total_bytes_processed))
+        return True
+
+    def validate_query(self, templated_query):
         try:
-            # Convert the Jinja templated SQL to a valid query
+            formatted_query = self._render_templated_query(templated_query)
+            return self._dry_run_query(formatted_query)
+            return True
+        except Exception as e:
+            logging.error(e)
+            return False
+
+
+    def validate_query_from_file(self, file_path):
+        try:
             f = open(file_path, "r")
             templated_query = f.read()
-
-            templated_query = templated_query.replace('params.', '')  # need this to get formatting correct
-            t = Template(templated_query)
-
-            formatted_query = t.render(self.params)
-
-            # Construct a BigQuery client object.
-            job_config = bigquery.QueryJobConfig(dry_run=True, use_query_cache=False)
-
-            # Start the query, passing in the extra configuration.
-            # query_job =
-            self.bq_client.query(
-                formatted_query,
-                job_config=job_config,
-            )  # Make an API request.
-
-            # A dry run query completes immediately.
-            # todo: decide if we should print anything
-            # print("This query will process {} bytes.".format(query_job.total_bytes_processed))
-            return True
+            formatted_query = self._render_templated_query(templated_query)
+            return self._dry_run_query(formatted_query)
         except Exception as e:
             logging.error(e)
             return False
