30ddf0e827f21f2fc9287e30fe325c90d1dfdee6
conall <conalldalydev@gmail.com>
2021-11-12 21:58:39 +0000

    fix bq result test

--- bigquery_validator/tests/bigquery_result_test.py
+++ bigquery_validator/tests/bigquery_result_test.py
@@ -9,13 +9,14 @@ class BigqueryResultTest(unittest.TestCase):
         query = "SELECT count(*) AS nrows FROM `{{ params.project }}.samples.github_timeline`"
 
         bqr = BigQueryResult(query)
-        self.assertIsNotNone(bqr.result())
+        print(bqr.result)
+        self.assertIsNotNone(bqr.result)
 
     def test_query_auto_executes_set_to_false_returns_empty_arrya(self):
         query = "SELECT count(*) AS nrows FROM `{{ params.project }}.samples.github_timeline`"
 
         bqr = BigQueryResult(query, auto_execute=False)
-        self.assertEquals(bqr.result(), [])
+        self.assertEquals(bqr.result, [])
 
     def test_query_metadata_is_not_none(self):
         query = "SELECT count(*) AS nrows FROM `{{ params.project }}.samples.github_timeline`"
