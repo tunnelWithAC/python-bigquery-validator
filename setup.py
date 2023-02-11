691f4bb87945c4d7f089b0e6cef05a54a3428d04
tunnelWithAC <conall.daly95@gmail.com>
2023-02-11 21:28:03 +0000

    update package version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="python-bigquery-validator",
-    version="0.0.10",
+    version="1.0.0",
     author="Conall Daly",
     author_email="conalldalydev@gmail.com",
     description="Python module for validating BigQuery sql queries with support for Jinja templated variables",
